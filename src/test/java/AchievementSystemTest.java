c607094bbadd2b25a96f0ecd43c27bd64eaf7e81
shinan <shinan@2dfire.com>
2018-02-24 14:31:53 +0800

    添加单元测试

--- src/test/java/AchievementSystemTest.java
+++ src/test/java/AchievementSystemTest.java
@@ -2,10 +2,18 @@
  * Copyright (C) 2009-2017 Hangzhou  Technology Co., Ltd.All rights reserved
  */
 
+import com.achievement.constant.AchievementConstant;
+import com.achievement.constant.AchievementEnum;
+import com.achievement.event.OrderEvent;
+import com.achievement.publisher.IEventClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * AchievementSystemTest
@@ -16,8 +24,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class AchievementSystemTest {
+    public static List<String> customerRegisterIds = new ArrayList<>();
+
+    static {
+        customerRegisterIds.add("a87337792a9e45dc82e970215d7229b9");
+    }
+
+    public String entityId = "99926498";
+    @Resource
+    IEventClient eventClient;
+
     @Test
-    public void Test(){
-        System.out.println("aa");
+    public void finishWelcomeAchievementTest() throws Exception {
+        OrderEvent orderEvent = new OrderEvent(entityId, customerRegisterIds, AchievementConstant.EventSource
+                .FROM_H5, AchievementEnum.EventTypeEnum.ORDER_EVENT.getType());
+        orderEvent.setAction(AchievementEnum.OrderActionEnum.SUBMIT.getCode());
+        orderEvent.setOrderType(AchievementConstant.OrderType.ORDER_SHOP);
+        eventClient.publish(orderEvent);
+        Thread.sleep(5000);
     }
 }
